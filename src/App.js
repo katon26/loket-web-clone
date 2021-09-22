4a8fe773deb81e59053fbf023e77909d8e853c4f
fadilayoga <fadilayoga@users.noreply.github.com>
2021-09-22 07:16:45 +0700

    add content

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import Grid from '@mui/material/Grid';
+import Featured from './components/Featured.js'
+import Selling from './components/Selling.js'
+import './App.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Grid
+      container
+      spacing={2}
+      direction="row"
+      justifyContent="center"
+    >
+      <Grid item lg={4} sm={12}>
+        <Featured title="Featured Event" />
+      </Grid>
+      <Grid item lg={4} sm={12}>
+        <Selling />
+      </Grid>
+    </Grid>
   );
 }
 
